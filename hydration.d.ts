155aaa8d694e480c872d1ff3115055574834adb0
Sebastian Weyrauch <git@dev-lab.de>
2020-02-06 18:10:51 +0100

    chore: updated typescript definitions

--- hydration.d.ts
+++ hydration.d.ts
@@ -1,3 +1,6 @@
+import { VueConstructor } from 'vue';
+
 export function hydrate(): Promise<void>;
 export function setGlobalHydrationData(globalData: Record<string, any>): void;
-export default function initHydration(globalData: Record<string, any>): void;
+export function setVueCallbackFunction(vueCallbackFunction: (Vue: VueConstructor) => void): void;
+export default function initHydration(globalData: Record<string, any>, vueCallbackFunction: (Vue: VueConstructor) => void): void;
